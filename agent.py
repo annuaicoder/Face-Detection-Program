e7ce6b2290789ea887c7b532a34a796d862b5823
Anas <153397401+annuaicoder@users.noreply.github.com>
2024-07-31 17:06:31 -0700

    Adding main python file

---
+++ agent.py
@@ -0,0 +1,44 @@
+import cv2
+from playsound import playsound
+
+# Load the pre-trained face detection model
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+# Initialize the webcam
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Error: Could not open webcam.")
+    exit()
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    
+    if not ret:
+        print("Error: Failed to capture image.")
+        break
+    
+    # Convert the frame to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    # Detect faces in the frame
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    
+    # Draw rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        print("Alert Detection")
+        # Play the alarm sound
+        playsound('alarm.mp3')
+        
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+    
+    # Break the loop on 'q' key press
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the webcam and close all windows
+cap.release()
+cv2.destroyAllWindows()
