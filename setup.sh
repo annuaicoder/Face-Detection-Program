7a41124f9e0981a909b6f649ccb18428b6c95546
Anas <153397401+annuaicoder@users.noreply.github.com>
2024-08-01 21:12:01 -0700

    Adding main setup file

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Run the Python script
+python agent.py
+
+echo "Setup complete. To run the script again, use: python agent.py"
